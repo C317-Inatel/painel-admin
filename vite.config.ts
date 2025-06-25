1c2171585b1f7663580f0fd032ba52d9d02f5bbe
Luís <luis.e@ges.inatel.br>
2025-06-25 20:20:50 -0300

    fix: home fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: true,
   },
+  base: '/painel-admin/',
   build: {
     sourcemap: mode === "development",
   },
-  base: "./",
 }));
