ca389d0d068e8ee1616aac6dccc9c5b06bcc1e98
Luís <luis.e@ges.inatel.br>
2025-06-25 20:34:43 -0300

    fix: home fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
-    host: true,
+    host: "::",
   },
   base: '/painel-admin/',
   build: {
